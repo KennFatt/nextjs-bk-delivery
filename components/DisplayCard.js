4e91bad2625a56fd5eba761ea833937dc3b5f580
Kennan Fattahillah <10755895+KennFatt@users.noreply.github.com>
2021-05-31 14:30:56 +0700

    Component `<DisplayCard>` enhancements.
    - Support responsive width
    - Order button in large screen devices

--- components/DisplayCard.js
+++ components/DisplayCard.js
@@ -1,11 +1,14 @@
 export default function DisplayCard({ title, imageSrc }) {
   return (
-    <div className="overflow-hidden bg-white shadow-xl rounded-xl cursor-pointer flex flex-col justify-center p-2">
+    <div className="overflow-hidden bg-white shadow-xl rounded-xl flex flex-col justify-center p-2 lg:p-4">
       {/* Image */}
       <img src={imageSrc} alt={title}></img>
       {/* Body */}
-      <div className="flex flex-col items-center justify-center font-primary text-branding-accent-secondary py-2 lg:py-4">
-        <p className="text-center">{title}</p>
+      <div className="flex justify-center lg:justify-between items-start font-primary text-branding-accent-secondary py-2 lg:py-4">
+        <p className="text-center lg:text-left lg:text-lg lg:pr-4">{title}</p>
+        <button className="hidden lg:block py-1 px-10 rounded-md bg-branding-accent-primary-darker font-secondary text-white font-semibold active:bg-branding-accent-primary">
+          ORDER
+        </button>
       </div>
     </div>
   );
