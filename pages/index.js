020d293006c9beea0d285620f1e80b2a70041aeb
Kennan Fattahillah <10755895+KennFatt@users.noreply.github.com>
2021-06-01 16:52:04 +0700

    Add background image and improve globals.css

--- pages/index.js
+++ pages/index.js
@@ -10,12 +10,17 @@ export default function Home() {
       <NavBar />
 
       {/* Item Grid */}
-      <main className="min-h-screen center-container bg-branding-light pb-7 lg:pb-11">
+      <main
+        className="min-h-screen bg-fixed bg-no-repeat bg-cover bg-branding-light pb-7 lg:pb-11"
+        style={{
+          backgroundImage:
+            "url('https://bkdelivery.co.id/static/website/img/BK_bg.jpg?5218847fa211')",
+        }}>
         <h3 className="text-2xl text-center py-7 font-primary text-branding-accent-secondary">
           OUR MENUS
         </h3>
 
-        <section className="grid w-full max-w-md min-h-screen grid-cols-2 gap-3 px-4 mx-auto lg:gap-4 lg:grid-cols-3 lg:max-w-5xl xs:px-0">
+        <section className="grid grid-cols-2 gap-3 px-4 center-container lg:gap-4 lg:grid-cols-3">
           {getProductCategories().map(({ id, name, thumbnailUrl }) => (
             <DisplayCard key={id} title={name} imageSrc={thumbnailUrl} />
           ))}
