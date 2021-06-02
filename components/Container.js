bf4ba91f6a5ddcacd568296fff07caccdbc6e681
Kennan Fattahillah <10755895+KennFatt@users.noreply.github.com>
2021-06-02 11:05:46 +0700

    Add new generic `<Container>` component.
    We'll use this component across pages and for SEO purposes.

---
+++ components/Container.js
@@ -0,0 +1,36 @@
+import Head from "next/head";
+
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+
+export default function Container(props) {
+  const { title, description } = props;
+  const meta = {
+    title: title || "Burger King - Clone",
+    author: "Kennan Fattah",
+    description:
+      description ||
+      "Burger King's delivery website clone built with Next.js and Tailwind CSS by @KennFatt",
+  };
+
+  return (
+    <>
+      <Head>
+        <title>{meta.title}</title>
+        <meta name="author" content={meta.author} />
+        <meta name="description" content={meta.description} />
+      </Head>
+
+      <NavBar />
+      <main
+        className="bg-branding-light pb-7 lg:pb-11 min-h-screen bg-fixed bg-no-repeat bg-cover"
+        style={{
+          backgroundImage:
+            "url('https://bkdelivery.co.id/static/website/img/BK_bg.jpg?5218847fa211')",
+        }}>
+        {props.children}
+      </main>
+      <Footer />
+    </>
+  );
+}
--- pages/index.js
+++ pages/index.js
@@ -1,34 +1,19 @@
 import DisplayCard from "@/components/DisplayCard";
-import NavBar from "@/components/NavBar";
-import Footer from "@/components/Footer";
+import Container from "@/components/Container";
 import { getProductCategories } from "@/lib/utils";
 
 export default function Home() {
   return (
-    <>
-      {/* Top Navigtion Bar */}
-      <NavBar />
+    <Container>
+      <h3 className="py-7 font-primary text-branding-accent-secondary text-2xl text-center">
+        OUR MENUS
+      </h3>
 
-      {/* Item Grid */}
-      <main
-        className="min-h-screen bg-fixed bg-no-repeat bg-cover bg-branding-light pb-7 lg:pb-11"
-        style={{
-          backgroundImage:
-            "url('https://bkdelivery.co.id/static/website/img/BK_bg.jpg?5218847fa211')",
-        }}>
-        <h3 className="text-2xl text-center py-7 font-primary text-branding-accent-secondary">
-          OUR MENUS
-        </h3>
-
-        <section className="grid grid-cols-2 gap-3 px-4 center-container lg:gap-4 lg:grid-cols-3">
-          {getProductCategories().map(({ id, name, thumbnailUrl }) => (
-            <DisplayCard key={id} title={name} imageSrc={thumbnailUrl} />
-          ))}
-        </section>
-      </main>
-
-      {/* Footer */}
-      <Footer />
-    </>
+      <section className="center-container lg:gap-4 lg:grid-cols-3 grid grid-cols-2 gap-3 px-4">
+        {getProductCategories().map(({ id, name, thumbnailUrl }) => (
+          <DisplayCard key={id} title={name} imageSrc={thumbnailUrl} />
+        ))}
+      </section>
+    </Container>
   );
 }
