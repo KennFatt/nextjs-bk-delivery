6f2511675cc1ca27c701a17db09d8b97a8265a15
Kennan Fattahillah <10755895+KennFatt@users.noreply.github.com>
2021-06-04 10:45:46 +0700

    Update `<DisplayCard>` component to be generic.
    - Extracting its styles into `globals.css`.
    - New utility function to support i18n currency.
    
    It will be used to display the card inside our `/menus` route.

--- components/DisplayCard.js
+++ components/DisplayCard.js
@@ -1,18 +1,34 @@
+import { toIDRFormat } from "@/lib/utils";
 import Link from "next/link";
 
-export default function DisplayCard({ navigateTo, title, imageSrc }) {
+export default function DisplayCard({
+  navigateTo,
+  title,
+  imageSrc,
+  productPrice,
+}) {
+  const cardTypeClass = productPrice ? "card__product" : "card__category";
+
   return (
     <Link href={navigateTo}>
       <a className="block">
-        <div className="rounded-xl lg:p-4 flex flex-col justify-center h-full p-2 overflow-hidden bg-white shadow-xl cursor-pointer">
+        <div className={`card ${cardTypeClass}`}>
           {/* Image */}
           <img src={imageSrc} alt={title}></img>
           {/* Body */}
-          <div className="lg:justify-between font-primary text-branding-accent-secondary lg:py-4 flex items-start justify-center py-2 space-x-2">
-            <h3 className="lg:text-left lg:text-lg text-center">{title}</h3>
-            <button className="lg:block bg-branding-accent-primary-darker font-secondary active:bg-branding-accent-primary-darker hover:bg-branding-accent-primary hidden px-8 py-1 font-semibold text-white rounded-md">
-              ORDER
-            </button>
+          <div>
+            {/* Title */}
+            <h3>{title}</h3>
+
+            {/* Price */}
+            {productPrice && <p>{toIDRFormat(productPrice || 0)}</p>}
+
+            {/* Button */}
+            {!productPrice && (
+              <button className="lg:block bg-branding-accent-primary-darker font-secondary active:bg-branding-accent-primary-darker hover:bg-branding-accent-primary hidden px-8 py-1 font-semibold text-white rounded-md">
+                ORDER
+              </button>
+            )}
           </div>
         </div>
       </a>
