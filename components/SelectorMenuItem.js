f12279d3d75e21da7aed546348d6d6e86ed795c9
Kennan Fattahillah <10755895+KennFatt@users.noreply.github.com>
2021-06-07 09:12:51 +0700

    Fix text position on `SelectorMenuItem` copmponent

--- components/SelectorMenuItem.js
+++ components/SelectorMenuItem.js
@@ -8,12 +8,12 @@ export default function SelectorMenuItem({
   return (
     <Link href={navigateTo}>
       <a
-        className={`rounded-xl block w-full px-3 hover:bg-opacity-75 transition duration-200 ${
+        className={`rounded-xl block w-full px-3 hover:bg-opacity-75 transition duration-200 min-h-[48px] ${
           isSelected
             ? "bg-branding-accent-primary text-white"
             : "bg-branding-accent-secondary-lighter"
         }`}>
-        <div className="flex items-center min-h-[48px] py-3">{displayName}</div>
+        <div className="flex items-center h-full py-3">{displayName}</div>
       </a>
     </Link>
   );
