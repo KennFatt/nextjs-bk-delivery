86baa09f1dbe3dd849750c5357cd418eceaa1549
Kennan Fattahillah <10755895+KennFatt@users.noreply.github.com>
2021-06-10 16:01:42 +0700

    Firebase Realtime Database integration.
    Behavior changes:
    
    - Each page that consume data from RTDB Endpoint will be revalidated after 60 seconds (ISR).
    - Each product item is now pre-generated (though I don't want to make this way, let's see if I could improve the cost of build time).
    - No more utility module and now `data-handler` module only has two helper functions. One for fetching the data and one to parsing (transforming) the data for UI/UX usages.
    
    Code changes:
    
    - Removing `data/menus.json` from source code.
    - Removing `utils` module.

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-import { loadData } from "@/lib/data-handler";
+import { fetchData, transformData } from "@/lib/data-handler";
 
 import DisplayCard from "@/components/DisplayCard";
 import Container from "@/components/Container";
 
-export default function Home(props) {
+export default function Home({ menusItem }) {
   return (
     <Container title="Home">
       <h2 className="font-primary text-branding-accent-secondary text-2xl text-center">
@@ -11,8 +11,7 @@ export default function Home(props) {
       </h2>
 
       <section className="center-container lg:gap-4 lg:grid-cols-3 grid grid-cols-2 gap-3 px-4">
-        {/* TODO: Impl navigation to the category page inside DisplayCard */}
-        {props.products.map(({ id, thumbnailUrl, displayName }) => (
+        {menusItem.map(({ id, thumbnailUrl, displayName }) => (
           <DisplayCard
             key={id}
             navigateTo={`/menus/${id}`}
@@ -26,21 +25,21 @@ export default function Home(props) {
 }
 
 export async function getStaticProps() {
-  const data = await loadData();
+  const data = await fetchData();
   if (!data) {
-    // TODO: handling empty data or a failure?
     return { notFound: true };
   }
 
-  const products = data.map(({ id, thumbnailUrl, displayName }) => ({
+  const { menus } = transformData(data);
+
+  const menusItem = menus.map(({ id, thumbnailUrl, displayName }) => ({
     id,
     thumbnailUrl,
     displayName,
   }));
 
   return {
-    props: {
-      products,
-    },
+    props: { menusItem },
+    revalidate: 60,
   };
 }
